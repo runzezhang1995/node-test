e08d87ff44854fcbfd9907b6e45e28558fa91789
unknown <runze.zhang.1995@gmail.com>
2018-04-13 01:30:00 +0800

    init commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'babel-polyfill';
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello');
+});
+
+const server = app.listen(8888,() => {
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
