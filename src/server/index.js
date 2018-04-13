394ca7a25dafc278883809f2e033ac0820ae2443
zhangrunze199511 <runze.zhang.1995@gmail.com>
2018-04-13 14:05:23 +0800

    add start script by using nodemon

--- src/server/index.js
+++ src/server/index.js
@@ -12,4 +12,4 @@ const server = app.listen(8888,() => {
     const host = server.address().address;
     const port = server.address().port;
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
-})
+});
